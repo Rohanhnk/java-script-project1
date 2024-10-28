deb5ffe1ae1e80fe902bdf873525293a493138a0
Rohan <rohanhnk08@gmail.com>
2024-10-29 01:25:40 +0530

    added animation when delete a todo list

--- app.js
+++ app.js
@@ -38,6 +38,13 @@ function deleteCheck(e){
    //DELETE TODO
    if(item.classList[0] === 'trash-btn'){
     const todo = item.parentElement;
-    todo.remove();
+    todo.classList.add("fall");
+   
    } 
+}
+
+//CEHCK MARK
+if(itme.classlist[0] ==="complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
 }
