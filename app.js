fbb8ee5728d2c8410d74e1a26f52978bf1b5916d
Rohan <rohanhnk08@gmail.com>
2024-10-31 18:32:52 +0530

    made completed button functional

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-
+const filterOption = document.querySelector(".filter-todo");
 //Evenet Listeners
 todoButton.addEventListener("click" , addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
+//Functions
 function addTodo(event) {
     //prevent form from submitting
     event.preventDefault();
@@ -52,4 +54,31 @@ if(item.classList[0] ==="complete-btn") {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
 }
+}
+
+function filterTodo(e) {
+    const todos = todoList.children;
+    console.log("61", todos)
+    Array.from(todos).forEach(function(todo){
+        switch(e.target.value){
+            
+case "all":
+    console.log("66",todo.style)
+    todo.style.display = "flex";
+    break;
+    case"completed":
+    if(todo.classList.contains("completed")){
+        todo.style.display = "flex";
+    } else {
+        todo.style.display = "none";
+    } break;
+    case "uncompleted":
+        if(!todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+        } else {
+            todo.style.todoList = "none";
+        }
+        break;
+        }
+    });
 }
